ac344dab0ac1f80afb18b8bf626fb8363582ad5a
Matt Grainger <grainger.pdx@gmail.com>
2018-09-07 11:01:52 -0700

    clean-up
    changed some comments, cleaned things up a little bit.

--- master_combined.py
+++ master_combined.py
@@ -1,4 +1,8 @@
 import pprint
+''' 
+Not currently using pprint's functionality, but it comes in handy for testing queries, and may be
+implemented in the final build 
+'''
 import json
 import requests
 import urllib.request
@@ -7,14 +11,16 @@ from time import sleep
 
 pp = pprint.PrettyPrinter(depth=50)
 
+# api keys are obfuscated, just throw your own keys in to get query functionality.
 g_api_key = "<google api key>"
+y_api_key = '<yahoo api key>'
+
 user_address = input("Please type in your location: ")
 
 def generate_url(address,api_key):
 	address = address.replace(" ","+")
 	address = address.replace(",","")
 	return address 
-#string out all non-word characters
 
 result = generate_url(user_address,g_api_key)
 
@@ -24,8 +30,9 @@ request = f"{endpoint}{result}&key={g_api_key}"
 g_response = urllib.request.urlopen(request).read()
 location = json.loads(g_response)
 
-# fix this... encountering bugs with converting to float, and it looks sloppy.
+
 # sometimes it doesn't want to convert to floats. adding a print statement magically makes this work.
+# re-factor this below code at some point
 
 g_user_lat = str([s['geometry']['location']['lat'] for s in location['results']]).replace('[','').replace(']','')
 print()
@@ -44,7 +51,7 @@ sleep(3)
 
 search_q = input("Please choose a category to search for: ")
 
-y_api_key = '<yahoo api key>'
+
 url = 'https://api.yelp.com/v3/businesses/search'
 headers = {'Authorization': f'Bearer {y_api_key}'}
 url_params ={'latitude': lat, 'longitude':
